76782a77fdd72d745ce7d11dc58d7cdc71ffe479
Guillermo Conoman <gille.91@hotmail.com>
2024-04-29 19:46:59 -0400

    first commit

--- ingredientes.py
+++ ingredientes.py
@@ -1,4 +1,5 @@
 #Lista de ingredientes para la pizza
-proteina = ['pollo', 'vacuno', 'carne vegetal']
-vegetales = ['tomate', 'aceitunas', 'champiñones']
-tipo_masa = ['tradicional', 'delgada'] 
+class Ingredientes():
+    proteina = ['pollo', 'vacuno', 'carne vegetal']
+    vegetales = ['tomate', 'aceitunas', 'champiñones']
+    tipo_masa = ['tradicional', 'delgada'] 
--- pizza.py
+++ pizza.py
@@ -1,4 +1,5 @@
-import ingredientes
+from ingredientes import Ingredientes
+
 #se crea la clase pizza
 class Pizza():
     #se establecen los atributos
@@ -17,34 +18,34 @@ class Pizza():
         self.vegetal_2 = 0
         self.masa = 0
 
-        self.carne = int(input(f"Ingrese el ingrediente proteico: \n1){ingredientes.proteina[0]} \n2){ingredientes.proteina[1]} \n3){ingredientes.proteina[2]}\n"))
+        self.carne = int(input(f"Ingrese el ingrediente proteico: \n1){Ingredientes.proteina[0]} \n2){Ingredientes.proteina[1]} \n3){Ingredientes.proteina[2]}\n"))
         if self.carne == 1:
-            self.carne = ingredientes.proteina[0]
+            self.carne = Ingredientes.proteina[0]
         elif self.carne == 2:
-            self.carne = ingredientes.proteina[1]
+            self.carne = Ingredientes.proteina[1]
         elif self.carne == 3:
-            self.carne = ingredientes.proteina[2]
-        self.vegetal_1 = int(input(f"Ingrese el ingrediente vegetal: \n1){ingredientes.vegetales[0]} \n2){ingredientes.vegetales[1]} \n3){ingredientes.vegetales[2]}\n"))
+            self.carne = Ingredientes.proteina[2]
+        self.vegetal_1 = int(input(f"Ingrese el ingrediente vegetal: \n1){Ingredientes.vegetales[0]} \n2){Ingredientes.vegetales[1]} \n3){Ingredientes.vegetales[2]}\n"))
         if self.vegetal_1 == 1:
-            self.vegetal_1 = ingredientes.vegetales[0]
+            self.vegetal_1 = Ingredientes.vegetales[0]
         elif self.vegetal_1 == 2:
-            self.vegetal_1 = ingredientes.vegetales[1]
+            self.vegetal_1 = Ingredientes.vegetales[1]
         elif self.vegetal_1 ==3:
-            self.vegetal_1 = ingredientes.vegetales[2]
-        self.vegetal_2 = int(input(f"Ingrese el ingrediente vegetal: \n1){ingredientes.vegetales[0]} \n2){ingredientes.vegetales[1]} \n3){ingredientes.vegetales[2]}\n"))
+            self.vegetal_1 = Ingredientes.vegetales[2]
+        self.vegetal_2 = int(input(f"Ingrese el ingrediente vegetal: \n1){Ingredientes.vegetales[0]} \n2){Ingredientes.vegetales[1]} \n3){Ingredientes.vegetales[2]}\n"))
         if self.vegetal_2 == 1:
-            self.vegetal_2 = ingredientes.vegetales[0]
+            self.vegetal_2 = Ingredientes.vegetales[0]
         elif self.vegetal_2 == 2:
-            self.vegetal_2 = ingredientes.vegetales[1]
+            self.vegetal_2 = Ingredientes.vegetales[1]
         elif self.vegetal_2 ==3:
-            self.vegetal_2 = ingredientes.vegetales[2]
-        self.masa = int(input(f"Ingrese el tipo de masa: \n1){ingredientes.tipo_masa[0]} \n2){ingredientes.tipo_masa[1]}\n"))
+            self.vegetal_2 = Ingredientes.vegetales[2]
+        self.masa = int(input(f"Ingrese el tipo de masa: \n1){Ingredientes.tipo_masa[0]} \n2){Ingredientes.tipo_masa[1]}\n"))
         if self.masa == 1:
-            self.masa = ingredientes.tipo_masa[0]
+            self.masa = Ingredientes.tipo_masa[0]
         elif self.masa == 2: 
-            self.masa = ingredientes.tipo_masa[1]
+            self.masa = Ingredientes.tipo_masa[1]
 
         p_final = [self.carne, self.vegetal_1, self.vegetal_2, self.masa]
 
-        self.valido = Pizza.validador(ingredientes.proteina + ingredientes.vegetales + ingredientes.tipo_masa, p_final)
+        self.valido = Pizza.validador(Ingredientes.proteina + Ingredientes.vegetales + Ingredientes.tipo_masa, p_final)
         return self.valido, self.carne, self.vegetal_1, self.vegetal_2, self.masa
